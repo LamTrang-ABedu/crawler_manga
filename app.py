4ce71f18687311b284b5e9cae68a97adf86a3819
Lam <LamTrang@abedu.vn>
2025-05-03 14:18:24 +0700

    a

--- app.py
+++ app.py
@@ -14,17 +14,15 @@ BASE_URL = "https://tranh18x.com"
 HEADERS = {"User-Agent": "Mozilla/5.0"}
 TIMEOUT = 15
 
-R2_BUCKET = "hopehub-storage"
-R2_ENDPOINT = "https://c9ac3686ee9b83a90c8f01bd6cd077fa.r2.cloudflarestorage.com"
-R2_ACCESS_KEY = os.getenv("R2_ACCESS_KEY")
-R2_SECRET_KEY = os.getenv("R2_SECRET_KEY")
+R2_ACCESS_KEY_ID = os.getenv('R2_ACCESS_KEY_ID')
+R2_SECRET_ACCESS_KEY = os.getenv('R2_SECRET_ACCESS_KEY')
+R2_ACCOUNT_ID = os.getenv('R2_ACCOUNT_ID')
+R2_BUCKET = 'hopehub-storage'
 
-s3 = boto3.client(
-    's3',
-    endpoint_url=R2_ENDPOINT,
-    aws_access_key_id=R2_ACCESS_KEY,
-    aws_secret_access_key=R2_SECRET_KEY,
-    region_name='auto'
+s3 = boto3.client('s3',
+    endpoint_url=f"https://{R2_ACCOUNT_ID}.r2.cloudflarestorage.com",
+    aws_access_key_id=R2_ACCESS_KEY_ID,
+    aws_secret_access_key=R2_SECRET_ACCESS_KEY
 )
 
 def upload_to_r2(key, data):
