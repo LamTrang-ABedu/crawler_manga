3fe7e12c554722b6424ae62061e80376f373c118
Lam Trang <lamtrang@abedu.vn>
2025-05-26 21:43:58 +0700

    Update metruyencv.py

--- utils/metruyencv.py
+++ utils/metruyencv.py
@@ -88,6 +88,7 @@ def crawl_chapter_content_batch(book, chapters):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
     # Thêm dòng này:
     user_data_dir = tempfile.mkdtemp()
     chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
